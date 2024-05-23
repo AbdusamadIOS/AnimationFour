ca813ce22f32f62ba8c1c3fcef5545dd7feeb808
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-05-23 20:35:18 +0500

    Initial Commit

---
+++ AnimationFour/AnimationFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationFourApp.swift
+//  AnimationFour
+//
+//  Created by Abdusamad Mamasoliyev on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
